b452dc9c8c43de6dfe78dadbf567475ff716f297
Oleg Lipskiy <lolego1601@gmail.com>
2019-03-09 22:50:47 +0200

    feat: add publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,4 +18,21 @@ dependencies {
     implementation("com.google.guava:guava:24.1-jre")
 }
 
+pluginBundle {
+    website = "https://github.com/acterics"
+    vcsUrl = "https://github.com/acterics/apk-dependency-graph-gradle-plugin"
+    tags = listOf("android", "apk-dependency-graph")
+}
+
+gradlePlugin {
+    plugins {
+        create("ApkDependencyGraphGenerator") {
+            id = "com.acterics.apk-dependency-graph-generator"
+            displayName = "Apk dependency graph generator"
+            description = "Gradle plugin for visualization your apk dependency graph"
+            implementationClass = "com.acterics.wmu.gradle.ApkDependencyGraphPlugin"
+            version = "0.9.0"
+        }
+    }
+}
 
